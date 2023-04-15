aac621104b948aa29888f6edf6bfd1352ec4e199
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2023-04-15 19:03:13 -0400

    Updating config error handling

--- src/config.rs
+++ src/config.rs
@@ -46,35 +46,35 @@ pub fn parse_cmd(cmd: &Value) -> anyhow::Result<ParsedCommand> {
             let prog = m.get("prog").unwrap_or(default_prog);
             let prog = prog
                 .as_str()
-                .ok_or(anyhow::anyhow!("Prog is not a string"))?;
+                .ok_or(anyhow::anyhow!("Invalid Config: `prog` is not a string"))?;
 
             let args = m.get("args").unwrap_or(default_args);
             let args = args
                 .as_sequence()
-                .ok_or(anyhow::anyhow!("Args is not a sequence"))?;
+                .ok_or(anyhow::anyhow!("Invalid Config: `args` is not a sequence"))?;
             let args = args
                 .iter()
                 .map(|a| {
                     a.as_str()
-                        .ok_or(anyhow::anyhow!("Arg {:?} is not a string", a))
+                        .ok_or(anyhow::anyhow!("Invalid Config: `arg` is not a string: {:?}", a))
                         .map(|s| s.to_string())
                 })
                 .collect::<anyhow::Result<Vec<String>>>()?;
 
             let cmd = m
                 .get("cmd")
-                .ok_or(anyhow::anyhow!("No cmd key in command"))?;
-            let cmd = cmd.as_str().ok_or(anyhow::anyhow!("Cmd is not a string"))?;
+                .ok_or(anyhow::anyhow!("Invalid Config: No `cmd` in config"))?;
+            let cmd = cmd.as_str().ok_or(anyhow::anyhow!("Invalid Config: `cmd` is not a string"))?;
 
             let pre_msg = m
                 .get("pre_msg")
-                .map(|v| v.as_str().ok_or(anyhow::anyhow!("pre_msg is not a string")))
+                .map(|v| v.as_str().ok_or(anyhow::anyhow!("Invalid Config: `pre_msg` is not a string")))
                 .transpose()?;
             let post_msg = m
                 .get("post_msg")
                 .map(|v| {
                     v.as_str()
-                        .ok_or(anyhow::anyhow!("post_msg is not a string"))
+                        .ok_or(anyhow::anyhow!("Invalid Config: `post_msg` is not a string"))
                 })
                 .transpose()?;
 
@@ -82,13 +82,13 @@ pub fn parse_cmd(cmd: &Value) -> anyhow::Result<ParsedCommand> {
                 .get("pre_cmds")
                 .map(|v| {
                     v.as_sequence()
-                        .ok_or(anyhow::anyhow!("pre_cmds is not a sequence"))
+                        .ok_or(anyhow::anyhow!("Invalid Config: `pre_cmds` is not a sequence"))
                 })
                 .transpose()?;
             let pre_cmds = pre_cmds
                 .map(|v| {
                     v.iter()
-                        .map(|v| v.as_str().ok_or(anyhow::anyhow!("pre_cmd is not a string")))
+                        .map(|v| v.as_str().ok_or(anyhow::anyhow!("Invalid Config: pre_cmd is not a string: {:?}", v)))
                         .collect::<anyhow::Result<Vec<&str>>>()
                         .map(|v| v.iter().map(|s| s.to_string()).collect())
                 })
@@ -98,7 +98,7 @@ pub fn parse_cmd(cmd: &Value) -> anyhow::Result<ParsedCommand> {
                 .get("post_cmds")
                 .map(|v| {
                     v.as_sequence()
-                        .ok_or(anyhow::anyhow!("post_cmds is not a sequence"))
+                        .ok_or(anyhow::anyhow!("Invalid Config: `post_cmds` is not a sequence"))
                 })
                 .transpose()?;
             let post_cmds = post_cmds
@@ -106,7 +106,7 @@ pub fn parse_cmd(cmd: &Value) -> anyhow::Result<ParsedCommand> {
                     v.iter()
                         .map(|v| {
                             v.as_str()
-                                .ok_or(anyhow::anyhow!("post_cmd is not a string"))
+                                .ok_or(anyhow::anyhow!("Invalid Config: `post_cmd` is not a string: {:?}", v))
                         })
                         .collect::<anyhow::Result<Vec<&str>>>()
                         .map(|v| v.iter().map(|s| s.to_string()).collect())
